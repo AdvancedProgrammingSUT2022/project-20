8c369e1f7e62cd0c0184c73f8f2d7b76ac2e93fc
amirali <me.amiralisalimi@gmail.com>
2022-04-20 19:27:19 +0430

    Add AbstractMenuView

--- civ/src/main/java/ir/ap/view/AbstractMenuView.java
+++ civ/src/main/java/ir/ap/view/AbstractMenuView.java
@@ -1,5 +1,69 @@
 package ir.ap.view;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import ir.ap.controller.GameController;
+import ir.ap.controller.UserController;
+import ir.ap.model.User;
+
 public abstract class AbstractMenuView {
-    
+    protected static final Scanner SCANNER = new Scanner(System.in);
+    protected static final UserController USER_CONTROLLER = new UserController();
+    protected static final GameController GAME_CONTROLLER = new GameController();
+
+    protected static User user;
+    protected static Menu currentMenu = Menu.LOGIN;
+
+    public static String constantCaseToCamelCase(String constCaseStr) {
+        String res = Arrays.stream(constCaseStr.split("_"))
+                .map(t -> t.substring(0, 1).toUpperCase() + t.substring(1).toLowerCase())
+                .collect(Collectors.joining(""));
+        return res.substring(0, 1).toLowerCase() + res.substring(1);
+    }
+
+    public static Matcher getCommandMatcher(String regex, String input) {
+        Matcher res = Pattern.compile(regex).matcher(input);
+        return (res.matches() ? res : null);
+    }
+
+    public static void run() {
+        currentMenu = Menu.LOGIN;
+        while ((currentMenu = currentMenu.runCommand()) != null) {
+        }
+    }
+
+    public Menu runCommand() {
+        if (!SCANNER.hasNextLine())
+            return null;
+        String input = SCANNER.nextLine();
+        Matcher matcher;
+        for (Enum<?> command : getCommands()) {
+            matcher = getCommandMatcher(command.toString(), input);
+            if (matcher != null) {
+                String methodName = constantCaseToCamelCase(command.name());
+                try {
+                    return (Menu) this.getClass().getMethod(methodName,
+                            Matcher.class)
+                            .invoke(this, matcher);
+                } catch (Exception ex) {
+                    ex.getCause().printStackTrace();
+                    throw new RuntimeException();
+                }
+            }
+        }
+        System.out.println("invalid command");
+        return currentMenu;
+    }
+
+    public abstract Enum<?>[] getCommands();
+
+    public abstract void enterMenu(Matcher matcher);
+
+    public abstract void exitMenu(Matcher matcher);
+
+    public abstract void showMenu(Matcher matcher);
 }
--- civ/src/main/java/ir/ap/view/Menu.java
+++ civ/src/main/java/ir/ap/view/Menu.java
@@ -1,5 +1,18 @@
 package ir.ap.view;
 
 public enum Menu {
+    LOGIN(new LoginMenuView()),
+    MAIN(new MainMenuView()),
+    GAME(new GameMenuView()),
+    PROFILE(new ProfileMenuView());
 
+    private final AbstractMenuView menuView;
+
+    Menu(AbstractMenuView menuView) {
+        this.menuView = menuView;
+    }
+
+    public Menu runCommand() {
+        return menuView.runCommand();
+    }
 }
