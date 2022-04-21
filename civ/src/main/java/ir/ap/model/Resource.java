08c1844ccdcd06a027d94f8d77971572299ee9bf
Davood Kareshki <dkareshki@gmail.com>
2022-04-21 20:17:53 +0430

    :Resources has been writed

--- civ/src/main/java/ir/ap/model/Resource.java
+++ civ/src/main/java/ir/ap/model/Resource.java
@@ -5,33 +5,36 @@ import java.util.Arrays;
 
 public enum Resource {
 
-    BANANAS(1,1,0,0,new ArrayList<>(Arrays.asList(new TerrainType.TerrainFeature[]{TerrainType.TerrainFeature.JUNGLE})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    CATTLE(2,1,0,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    DEER(3,1,0,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    FISH(4,2,0,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    SHEEP(5,1,0,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    WHEAT(6,1,0,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    ALUMINUM(7,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    COAL(8,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    HORSES(9,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    IRON(10,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    OIL(11,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    URANIUM(12,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    COTTON(13,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    DYES(14,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    FURS(15,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    GEMS(16,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    GOLD(17,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    INCENSE(18,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    IVORY(19,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    MARBLE(20,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    PEARLS(21,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    SILK(22,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    SILVER(23,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    SPICES(24,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    SUGAR(25,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    WHALES(26,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{}))),
-    WINE(27,0,1,0,new ArrayList<>(Arrays.asList(new TerrainType[]{})),new ArrayList<>(Arrays.asList(new Improvement[]{})),new ArrayList<>(Arrays.asList(new Technology[]{})));
+    ///// Bonus Resources
+    BANANAS(1,1,0,0,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TerrainFeature.FOREST})),Improvement.PLANTATION,null),
+    CATTLE(2,1,0,0,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.GRASSLAND})),Improvement.PASTURE,null),
+    DEER(3,1,0,0,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TerrainFeature.FOREST,TerrainType.TUNDRA,TerrainType.HILL})),Improvement.CAMP,null),
+    SHEEP(4,1,0,0,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.DESERT,TerrainType.GRASSLAND,TerrainType.DESERT,TerrainType.HILL})),Improvement.PASTURE,null),
+    // FISH(5,2,0,0,new ArrayList<>(Arrays.asList(new Enum[]{})),Improvement.,new ArrayList<>(Arrays.asList(new Technology[]{}))),
+    WHEAT(6,1,0,0,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.PLAINS,TerrainType.TerrainFeature.FLOOD_PLAINS})),Improvement.FARM,null),
+
+    ////// Strategic Resources
+    //ALUMINUM(7,0,1,0,new ArrayList<>(Arrays.asList(new Enum[]{})),Improvement.,new ArrayList<>(Arrays.asList(new Technology[]{}))),
+    COAL(8,0,1,0,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.PLAINS, TerrainType.HILL, TerrainType.GRASSLAND})),Improvement.MINE,Technology.SCIENTIFIC_THEORY),
+    HORSES(9,0,1,0,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TUNDRA,TerrainType.PLAINS,TerrainType.GRASSLAND})),Improvement.PASTURE,Technology.ANIMAL_HUSBANDRY),
+    IRON(10,0,1,0,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TUNDRA,TerrainType.PLAINS, TerrainType.DESERT,TerrainType.HILL,TerrainType.GRASSLAND,TerrainType.SNOW})),Improvement.MINE,Technology.IRONWORKING),
+    //OIL(11,0,1,0,new ArrayList<>(Arrays.asList(new Enum[]{})),Improvement.,new ArrayList<>(Arrays.asList(new Technology[]{}))),
+    //URANIUM(12,0,1,0,new ArrayList<>(Arrays.asList(new Enum[]{})),Improvement.,new ArrayList<>(Arrays.asList(new Technology[]{}))),
+    COTTON(13,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.PLAINS,TerrainType.DESERT,TerrainType.GRASSLAND})),Improvement.PLANTATION,null),
+    DYES(14,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TerrainFeature.FOREST, TerrainType.TerrainFeature.JUNGLE})),Improvement.PLANTATION,null),
+    FURS(15,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TerrainFeature.FOREST, TerrainType.TUNDRA})),Improvement.CAMP,null),
+    GEMS(16,0,0,3,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TerrainFeature.JUNGLE,TerrainType.TUNDRA,TerrainType.PLAINS,TerrainType.DESERT,TerrainType.GRASSLAND,TerrainType.HILL})),Improvement.MINE,null),
+    GOLD(17,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.PLAINS,TerrainType.DESERT,TerrainType.GRASSLAND,TerrainType.HILL})),Improvement.MINE,null),
+    INCENSE(18,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.PLAINS,TerrainType.DESERT})),Improvement.PLANTATION,null),
+    IVORY(19,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.DESERT})),Improvement.CAMP,null),
+    MARBLE(20,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TUNDRA,TerrainType.PLAINS,TerrainType.DESERT,TerrainType.GRASSLAND,TerrainType.HILL})),Improvement.MINE,null),
+    //PEARLS(21,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{})),Improvement.,null),
+    SILK(22,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TerrainFeature.FOREST})),Improvement.PLANTATION,null),
+    SILVER(23,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TUNDRA,TerrainType.DESERT,TerrainType.HILL})),Improvement.MINE,null),
+    //SPICES(24,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{})),Improvement.,null),
+    SUGAR(25,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{TerrainType.TerrainFeature.FLOOD_PLAINS, TerrainType.TerrainFeature.MARSH})),Improvement.PLANTATION,null);
+    //WHALES(26,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{})),Improvement.,null),
+    //WINE(27,0,0,2,new ArrayList<>(Arrays.asList(new Enum[]{})),Improvement.,null);
     public enum ResourceType {
         BONUS,
         STRATEGIC,
@@ -42,11 +45,11 @@ public enum Resource {
     private int foodYield;
     private int productionYield;
     private int goldYield;
-    private ArrayList<TerrainType> canBeFoundOn;
-    private ArrayList<Improvement> improvementsRequired;
-    private ArrayList<Technology> technologyRequired;
+    private ArrayList<Enum<?>> canBeFoundOn;
+    private Improvement improvementsRequired;
+    private Technology technologyRequired;
 
-    Resource(int id, int foodYield, int productionYield, int goldYield, ArrayList<TerrainType> canBeFoundOn, ArrayList<Improvement> improvementsRequired, ArrayList<Technology> technologyRequired)
+    Resource(int id, int foodYield, int productionYield, int goldYield, ArrayList<Enum<?>> canBeFoundOn, Improvement improvementsRequired, Technology technologyRequired)
     {
         this.id = id;
         this.foodYield = foodYield;
