1a39dea65ff7d97eab368177eb3f9f6eb71aa7ed
SbziAM <Amirmohamad.sabzi007@gmail.com>
2022-04-22 18:42:39 +0430

    Unit

--- civ/src/main/java/ir/ap/model/Unit.java
+++ civ/src/main/java/ir/ap/model/Unit.java
@@ -1,5 +1,70 @@
 package ir.ap.model;
 
+import ir.ap.model.UnitType.UnitAction;
+
 public class Unit {
+    UnitType unitType ;
+    UnitAction unitAction ;
+    int mp ;
+    int hp ;
+    Civilization civilization ;
+    Tile tile ;
+    
+    public Unit(UnitType unitType, UnitAction unitAction, int mp, int hp, Civilization civilization, Tile tile) {
+        this.unitType = unitType;
+        this.unitAction = unitAction;
+        this.mp = mp;
+        this.hp = hp;
+        this.civilization = civilization;
+        this.tile = tile;
+    }
+
+    public UnitType getUnitType() {
+        return unitType;
+    }
+
+    public void setUnitType(UnitType unitType) {
+        this.unitType = unitType;
+    }
+
+    public UnitAction getUnitAction() {
+        return unitAction;
+    }
+
+    public void setUnitAction(UnitAction unitAction) {
+        this.unitAction = unitAction;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public Civilization getCivilization() {
+        return civilization;
+    }
+
+    public void setCivilization(Civilization civilization) {
+        this.civilization = civilization;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
     
 }
