6d1b675fe5b7f809b5b328bb2cdc5b54a99d72fd
amirali <me.amiralisalimi@gmail.com>
2022-05-02 02:29:15 +0430

    Dont make commands lowercase

--- civ/src/main/java/ir/ap/controller/UserController.java
+++ civ/src/main/java/ir/ap/controller/UserController.java
@@ -79,6 +79,7 @@ public class UserController implements JsonResponsor, AutoCloseable {
             Reader usersReader = new FileReader(PLAYERS_CONF_FILE);
             User[] curUsers = GSON.fromJson(usersReader, User[].class);
             for (User user : curUsers) {
+                user.setLogin(false);
                 User.addUser(user);
             }
             usersReader.close();
